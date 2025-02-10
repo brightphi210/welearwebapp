80843784f1eea3dcaa7c6c007ab0cef1161b35c2
Bright Philip <chibuzorphilip2001@gmail.com>
2025-02-10 18:24:00 +0100

    Fix All issues

--- src/pages/access/instructor/InstructorHome.tsx
+++ src/pages/access/instructor/InstructorHome.tsx
@@ -1,6 +1,5 @@
 import { CustomizedButtonMain } from '@/Compnents/UI/CustomizedButton'
 import { useAuth } from '@/Providers/AuthContext'
-import React from 'react'
 
 const InstructorHome = () => {
   const {handleLogout} = useAuth()
--- src/pages/access/student/StudentBar.tsx
+++ src/pages/access/student/StudentBar.tsx
@@ -1,12 +1,11 @@
-import { profileImage } from '@/Compnents/images/imagePath'
-import React from 'react'
-import { IoMdClose } from 'react-icons/io'
-import { RiMenu2Fill } from 'react-icons/ri'
+// import { profileImage } from '@/Compnents/images/imagePath'
+// import { IoMdClose } from 'react-icons/io'
+// import { RiMenu2Fill } from 'react-icons/ri'
 
 const StudentBar = () => {
   return (
     <div>
-        <div className="flex fixed pt-4 pb-4 w-full z-50 bg-white items-center justify-between border-b border-neutral-200 lg:px-10  px-5">
+        {/* <div className="flex fixed pt-4 pb-4 w-full z-50 bg-white items-center justify-between border-b border-neutral-200 lg:px-10  px-5">
             <div className="flex gap-5 items-center">
 
                 <div className="lg:hidden block " onClick={toggleDrawer}>
@@ -21,7 +20,7 @@ const StudentBar = () => {
             <div className="w-10 h-10">
             <img src={profileImage?.porfileImg || "/placeholder.svg"} alt="Profile" />
             </div>
-        </div>
+        </div> */}
     </div>
   )
 }
